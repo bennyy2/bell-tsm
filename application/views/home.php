2571ea4d39b3f30d9397592dbb24e0dbec1d52d7
Benjarat Chantaprasert <sakjura@gmail.com>
2018-07-19 16:43:17 +0700

    initial

---
+++ application/views/home.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Bell T.S.M. Corporation Limited</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+        <link href="<? echo base_url();?>css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-header">
+            <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+                <!-- Brand -->
+                <a class="navbar-brand" href="#">T.S.M.</a>
+
+                <!-- Links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+
+                    <!-- Dropdown -->
+                    <!--                    <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                                Dropdown link
+                                            </a>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item" href="#">Link 1</a>
+                                                <a class="dropdown-item" href="#">Link 2</a>
+                                                <a class="dropdown-item" href="#">Link 3</a>
+                                            </div>
+                                        </li>-->
+                </ul>
+
+            </nav>
+
+            <img src="images/bellheader.png" class="img-fluid" alt="Bell logo">
+        </div>
+        <div class="container" >
+            <br>
+            <h1 style="text-align: center">Bell T.S.M. Corporation LTD.</h1>
+            <hr>
+            <div class="card bg-dark text-white" style="padding: 50px">
+                <h2>Our Website is under construction</h2>
+                <p>During this process, you can reach us via following contact details.</p>
+                <p>Email : belltsm.belltsm.com<br>
+                    TEL : (66)0-2223-6691, (66)0-2223-6205
+                </p>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-sm-4">
+                    <img class="float-center" width="20%" src="images/earth-globe.png"/>
+                    <br><br>
+                    <h3>Our business</h3>
+                    <p>Manufacturer of Automotive Rubber Part,  Engine Mounting, Air-Oil-Water Hose, Hose and Rubber MatalBonding</p> 
+
+                </div>
+                <div class="col-sm-4">
+                    <img class="float-center" width="20%" src="images/earth-globe.png"/>
+                    <br><br>
+                    <h3>Product</h3>
+                    <p>Lorem ipsum dolor..</p>
+                    <p>Ut enim ad..</p>
+                </div>
+                <div class="col-sm-4">
+                    <img class="float-center" width="20%" src="images/earth-globe.png"/>
+                    <br><br>
+                    <h3>Quality control & Reliability</h3> 
+                    <p>Lorem ipsum dolor..</p>
+                    <p>Ut enim ad..</p>
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+        <footer class="footer-copyright bg-dark py-3 mb-2 text-white ">
+            <p>Copyright &copy; 2018 Bell T.S.M. Corporation Limited; All rights reserved. Icon made by Freepik from www.flaticon.com </p>
+        </footer>
+    </body>
+</html>
+<script type="text/javascript" src="<? echo base_url();?>js/script.js"></script>
